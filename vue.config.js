081984ec696e0e5c9ad11c7dddf2b5bc0312cc41
Berend Swennenhuis <berendswennenhuis@gmail.com>
2019-06-09 23:12:15 +0200

    dist

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = {
   publicPath: '/web-bluetooth-playground/',
   // publicPath: 'web-bluetooth-playground',
   // baseUrl: 'web-bluetooth-playground',
-  // indexPath: '../index.html'
+  indexPath: '../index.html'
  } 
